411c629a9992a9ffde8a62ea0d8d22ee33374e4e
arfanur04 <arfanur04@gmail.com>
2024-02-20 09:21:14 +0600

    add event listener info updated

--- scripts/index.js
+++ scripts/index.js
@@ -2,32 +2,23 @@ let seatArray = [];
 document
 	.getElementById("seat-container")
 	.addEventListener("click", function (e) {
-		// const seatID = e.target.id;
 		if (e.target.id.length === 2 && seatArray.length !== 4) {
 			const seatID = e.target.id;
-
-			if (seatArray.length === 0) {
+			if (!seatArray.includes(seatID)) {
 				seatArray.push(seatID);
 				setLeftSeat();
 				appendTicket(seatID);
 				totalPrice(seatArray.length);
 				grandTotal(seatArray.length);
-			} else if (!seatArray.includes(seatID)) {
-				seatArray.push(seatID);
-				setLeftSeat();
-				appendTicket(seatID);
-				totalPrice(seatArray.length);
-				grandTotal(seatArray.length);
-			}
-			setBackgroundColorById(seatID);
-			// selected seat count and display on ui
-			setTextElementValueById("selected-seat-length", seatArray.length);
-			if (seatArray.length === 4) {
-				console.log("You have reached your limit");
-				applyButton();
+				setBackgroundColorById(seatID);
+				// count selected seat and display on ui
+				setTextElementValueById("selected-seat-length", seatArray.length);
 			}
 		}
-
+		if (seatArray.length === 4) {
+			console.log("You have reached your limit");
+			applyButton();
+		}
 		if (seatArray.length) {
 			console.log(seatArray);
 		}
